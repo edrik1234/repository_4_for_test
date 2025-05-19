851bc19d9ab98568a1cf2e869df7d3449b7f40f7
Edrian <razer4832@gmail.com>
2025-05-19 21:15:57 +0300

    the name of student changed from alex to edrian and field of study changed from english to electronics

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Student import Student
 from Employee import Employee
 
-student = Student("Alex", 30, "English", 8 ,900)
+student = Student("edrian", 30, "electronics", 8 ,900)
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
