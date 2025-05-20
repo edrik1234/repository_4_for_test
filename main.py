6dabe27db118f7b8d62db3986ac05035c441978d
edrik1234 <edrik4832@gmail.com>
2025-05-20 08:25:50 +0300

    Update main.py
    print  this is from programmer 2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from Student import Student
 from Employee import Employee
-
+print("this is from programmer 2")
 student = Student("edrian", 30, "electronics", 8 ,900)
 # student.foo()
-print("this is from programmer 2")
+
 employee = Employee("John", 40, "Software Engineer", 45000)
 people = [student, employee]
 for person in people:
