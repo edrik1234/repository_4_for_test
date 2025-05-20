90e5c369c20ffecf206c3fedb346a4cba5c8f6aa
Edrian <razer4832@gmail.com>
2025-05-20 09:18:31 +0300

    changed main from 123 to 12345

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Student import Student
 from Employee import Employee
-print("this is from programmer 2", "123")
+print("this is from programmer 2", "12345")
 student = Student("edrian", 30, "electronics", 8 ,900)
 # student.foo()
 print("this is from programmer 1")
