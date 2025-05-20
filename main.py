b443efdc1fbe11166c1da776af3c5dc548d7fb9b
edrik1234 <edrik4832@gmail.com>
2025-05-20 19:11:47 +0300

    Update main.py
    qrvcxsrqrqdf

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from Student import Student
 from Employee import Employee
-print("this is from programmer 2", "12345")
+print("this is from programmer 2 is hara", "12345")
 student = Student("edrian", 30, "electronics", 8 ,900)
 print("this is from programmer 2 hi hi"< "lol")
 student = Student("edrian", 30, "electronics", 8,900)
 # student.foo()
-print("this is from programmer 1 hi yepta")
+print("this is from programmer 1 hi yepta is hara")
 employee = Employee("John", 40, "Software Engineer", 45000)
 people = [student, employee]
 for person in people:
